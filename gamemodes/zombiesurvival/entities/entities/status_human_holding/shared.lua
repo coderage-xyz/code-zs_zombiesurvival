f9968f5bc77ca9811f34dbc21a6d202181f8cd93
John Connor <jckusho@gmail.com>
2023-03-14 23:30:25 -0400

    Stopped perk-related console spam from carrying props

--- gamemodes/zombiesurvival/entities/entities/status_human_holding/shared.lua
+++ gamemodes/zombiesurvival/entities/entities/status_human_holding/shared.lua
@@ -12,7 +12,7 @@ function ENT:Move(pl, mv)
 		--if objectphys:IsValid() then
 			mv:SetMaxSpeed(math.max(
 				mv:GetMaxSpeed() / 4,
-				mv:GetMaxSpeed() - self:GetObjectMass() * CARRY_SPEEDLOSS_PERKG * pl.PropCarrySlowMul)
+				mv:GetMaxSpeed() - self:GetObjectMass() --[[* CARRY_SPEEDLOSS_PERKG * pl.PropCarrySlowMul]])
 			)
 			mv:SetMaxClientSpeed(mv:GetMaxSpeed())
 		--end
